678637ff44b67fdee7d5fdc9b7359d67407fa022
Kuan Wei Li <fatrabitree@gmail.com>
2020-10-14 00:22:36 -0500

    first commit

---
+++ sho.rt/Pages/Index.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace sho.rt.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public string Message { get; private set; } = "";
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult OnGet(string param)
+        {
+            if (param == null)
+            {
+                return Page();
+            }
+            else
+            {
+                return Redirect("https://www.google.com");
+            }
+        }
+
+        public IActionResult OnPost(string url)
+        {
+            if (url == null)
+            {
+                Message = "url is empty";
+            }
+            else if (!Uri.IsWellFormedUriString(url, UriKind.Absolute))
+            {
+                Message = "url is invalid";
+            }
+            else
+            {
+                Message = url;
+            }
+            return Page();
+        }
+    }
+}
