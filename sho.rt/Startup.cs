44578ac4a14b92f58603e42b9e9674f4660399b3
Kuan Wei Li <fatrabitree@gmail.com>
2020-10-30 23:47:09 -0500

    Add DB connection retry

--- sho.rt/Startup.cs
+++ sho.rt/Startup.cs
@@ -29,8 +29,10 @@ namespace sho.rt
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DB")));
+            services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("DB"),
+                providerOptions => providerOptions.EnableRetryOnFailure())
+                );
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
